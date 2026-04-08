a742d713fde97492590fddec7a3c1d609082e204
Bartosz <Bartosz@255611.com>
2026-04-08 15:17:11 +0200

    9. widocznosc atrybutow klas

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -7,7 +7,7 @@ public class DefaultCountingOutRhymer {
 
     final private int[] NUMBERS = new int[ARRAY_SIZE];
 
-    public int total = -1;
+    private int total = -1;
 
     public void countIn(int in) {
         if (!isFull())
